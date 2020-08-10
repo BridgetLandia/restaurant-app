0294bc65c1b183716b89e139756fedd1002455d2
Brigitta Tóth <52370131+BridgetLandia@users.noreply.github.com>
2020-08-10 13:16:31 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyASdJkzdYGb9U38gJHreVRAhJrLO-KM4QE",
-    authDomain: "restaurant-4d6eb.firebaseapp.com",
-    databaseURL: "https://restaurant-4d6eb.firebaseio.com",
-    projectId: "restaurant-4d6eb",
-    storageBucket: "restaurant-4d6eb.appspot.com",
-    messagingSenderId: "770472311141",
-    appId: "1:770472311141:web:9928b6dcfcb389dd4b098c"
+    apiKey: 
+    authDomain:
+    databaseURL: 
+    projectId: 
+    storageBucket: 
+    messagingSenderId: 
+    appId: 
 }
 
 // Initialize Firebase
